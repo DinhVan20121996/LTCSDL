d4fc4716c55931c18607ae80c1138d1e5c255365
DinhVan20121996 <dinhngocvan112233@gmail.com>
2017-10-04 11:02:24 +0700

    Bai tap Tuan 3 CSDL

---
+++ Bai4_B3/Bai4_B3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Bai4_B3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txttenloai = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmaloai = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.btthem = new System.Windows.Forms.Button();
+            this.dsGV1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dsGV1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txttenloai);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtmaloai);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(397, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại Sản Phẩm";
+            // 
+            // txttenloai
+            // 
+            this.txttenloai.Location = new System.Drawing.Point(101, 52);
+            this.txttenloai.Multiline = true;
+            this.txttenloai.Name = "txttenloai";
+            this.txttenloai.Size = new System.Drawing.Size(289, 30);
+            this.txttenloai.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên Loại";
+            // 
+            // txtmaloai
+            // 
+            this.txtmaloai.Location = new System.Drawing.Point(101, 19);
+            this.txtmaloai.Multiline = true;
+            this.txtmaloai.Name = "txtmaloai";
+            this.txtmaloai.Size = new System.Drawing.Size(289, 27);
+            this.txtmaloai.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Loại";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btsua);
+            this.groupBox2.Controls.Add(this.btxoa);
+            this.groupBox2.Controls.Add(this.btthem);
+            this.groupBox2.Location = new System.Drawing.Point(0, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 57);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(317, 19);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(75, 23);
+            this.btsua.TabIndex = 4;
+            this.btsua.Text = "Sửa";
+            this.btsua.UseVisualStyleBackColor = true;
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(175, 19);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(75, 23);
+            this.btxoa.TabIndex = 3;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(26, 19);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(75, 23);
+            this.btthem.TabIndex = 2;
+            this.btthem.Text = "Thêm";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // dsGV1
+            // 
+            this.dsGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dsGV1.Location = new System.Drawing.Point(2, 184);
+            this.dsGV1.Name = "dsGV1";
+            this.dsGV1.Size = new System.Drawing.Size(397, 169);
+            this.dsGV1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 354);
+            this.Controls.Add(this.dsGV1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Loại Sản Phẩm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dsGV1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtmaloai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.TextBox txttenloai;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.DataGridView dsGV1;
+    }
+}
+
---
+++ Bai4_B3/Bai4_B3/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Bai4_B3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sc = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cnstr = "Server = . ; Database = QLBanHang; Integrated Security = true ";
+            sc = new SqlConnection(cnstr);
+            dsGV1.DataSource = LoadData();
+        }
+        private void Connect()
+        {  
+            try
+            {
+                if (sc != null && sc.State != ConnectionState.Open)
+                    sc.Open();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Kết nối đã mở hoặc chưa có CSDL\n" + ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Mật khẩu CSDL sai\n" + ex.Message);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                MessageBox.Show("Có 2 CSDL giống nhau\n" + ex.Message);
+            }
+        }
+        private void DisConnect()
+        {
+            if (sc != null && sc.State != ConnectionState.Closed)
+            {
+                sc.Close();
+            }
+        }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            string Query = "INSERT FROM LoaiSP WHERE MaLoaiSP =" + txtmaloai.Text; 
+        }
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            string Query = "DELETE FROM LoaiSP WHERE MaLoaiSP =" + txtmaloai.Text;
+            Connect();
+            SqlCommand cmd = new SqlCommand(Query, sc);
+            int NumberOfRows = 0;
+            NumberOfRows = cmd.ExecuteNonQuery();
+            dsGV1.DataSource = LoadData();
+            MessageBox.Show("Đã xóa dòng : " + NumberOfRows.ToString());
+            DisConnect();
+            
+        }
+        private List<object> LoadData()
+        {
+            Connect();
+            string sql = "SELECT * FROM LoaiSP";
+            List<object> list = new List<object>();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, sc);
+                SqlDataReader Reader = cmd.ExecuteReader();
+                int id; 
+                string name;
+                while (Reader.Read())
+                {
+                    id = Reader.GetInt32(0);
+                    name = Reader.GetString(1);
+                    var prod = new
+                    {
+                        MaLoaiSP = id,
+                        TenLoaiSP = name
+                    };
+                    list.Add(prod);
+                }
+                Reader.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DisConnect(); 
+            }
+            return list;   
+        }
+    }
+}
